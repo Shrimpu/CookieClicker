95a376f1fe65342b212ab84a56aeba931aa9c42d
Joar Andersson <joar.andersson@learnet.se>
2019-03-16 09:02:18 +0100

    Unused Script added.

---
+++ Assets/Scripts/AlternativeBuyIdea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlternativeBuyIdea : MonoBehaviour
+{
+    public enum BuyAmounts { buy1, buy10, buy100 };
+    BuyAmounts buyAmount = new BuyAmounts();
+
+    // public Upgrade[] upgrades
+
+    public void ChangeBuyAmount(BuyAmounts i) // basically an int
+    {
+        buyAmount = i;
+    }
+
+    public void Buy(int upgrade)
+    {
+        int upgradesToPurchase = (int)Mathf.Pow(10, (int)buyAmount); // 10^0 = 1, 10^1 = 10, 10^2 = 100.
+        for (int i = 0; i < upgradesToPurchase; i++)
+        {
+            // purchase upgrade[i]
+        }
+    }
+}
